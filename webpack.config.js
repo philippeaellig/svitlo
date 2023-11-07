597cc6f344be9fe7b88317cf3e3993b8d2f89e34
Philippe Aellig <philippe@aelligs.ch>
2023-11-07 16:19:03 +0100

    init

--- webpack.config.js
+++ webpack.config.js
@@ -53,6 +53,18 @@ Encore
         config.corejs = '3.23';
     })
 
+    .copyFiles({
+        from: './assets/images',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
     // enables Sass/SCSS support
     .enableSassLoader()
 
